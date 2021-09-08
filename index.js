539307e07e122906e7530987533d2ff23f4e4a2f
Benjamin Lebron <benjaminnlebron@gmail.com>
2021-09-08 14:02:02 -0400

    failed javascript logic commented out

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let outputEl = document.querySelector(".output-El");
+let outputEl = document.querySelector(".output-el");
 //let clickContainer = document.querySelector(".click-container")
 let clickContainerLength = document.querySelectorAll(".click-container").length;
 let clickContainer = document.querySelectorAll(".click-container")
 
-for (let i = 0; i < clickContainerLength ; i++) {
-   clickContainer[i].addEventListener("click", function() {
-        if () {
-            
-        }
-    });
+
+/* 
+function renderOutput() {
+  
+    for (let i = 0; i < clickContainerLength ; i++) {
+        clickContainer[i].addEventListener("click", function () {
+         if (clickContainer[i].id === "AS") {
+             
+         }
+        });
+     }
 }
+ */
 
