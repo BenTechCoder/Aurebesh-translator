8f33c61a65afbd239531fd7d999aff64f5a8ce70
Benjamin Lebron <benjaminnlebron@gmail.com>
2021-09-08 13:21:54 -0400

    added aurebesh grid

---
+++ index.js
@@ -0,0 +1,13 @@
+let outputEl = document.querySelector(".output-El");
+//let clickContainer = document.querySelector(".click-container")
+let clickContainerLength = document.querySelectorAll(".click-container").length;
+let clickContainer = document.querySelectorAll(".click-container")
+
+for (let i = 0; i < clickContainerLength ; i++) {
+   clickContainer[i].addEventListener("click", function() {
+        if () {
+            
+        }
+    });
+}
+
