868b5bcc1996534a29ae2d217ebe9ce5d18cd6a5
Benjamin Lebron <benjaminnlebron@gmail.com>
2021-09-10 16:18:08 -0400

    Complete functionality and complete HTML

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ let clickContainerLength = document.querySelectorAll(".click-container").length;
 let clickContainer = document.querySelectorAll(".click-container")
 
 
-
 function renderOutput(str) {
-
     let output = str;
     outputEl.textContent += output;
 }
 
+function deleteOutputAll() {
+    outputEl.textContent = " ";
+}
+
+function backspace() {
+   let currentstr = outputEl.innerHTML;
+  let slicer = currentstr.substring(0, currentstr.length - 1);
+   outputEl.textContent = slicer;
+}
+
+function space() {
+    outputEl.textContent += " ";
+}
